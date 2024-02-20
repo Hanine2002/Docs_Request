ecad56657a00bb56f3200880d0cbc7b871645366
Hanine2002 <hanine.nadia@ensam-casa.ma>
2024-02-20 15:11:22 +0100

    All project

---
+++ laravelBack/database/seeders/ConventionSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ConventionStage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConventionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+    }
+}
---
+++ laravelBack/database/seeders/ReleveSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ReleveNotes;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReleveSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       
+    }
+}
